1805d33936b8ec0f1c84863c417d642a10e31b97
jjuncoder <jjuncoder@gmail.com>
2019-04-08 18:23:05 +0900

    3주차 까지 만든 프로젝트 업로드

---
+++ Project/FlappyYonsei/Assets/Script/BGScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    public float nextPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnBecameInvisible()
+    {
+        //Debug.Log("Invisible!");
+        transform.position = new Vector3(
+            transform.position.x + nextPosition, transform.position.y, 0);
+    }
+}
