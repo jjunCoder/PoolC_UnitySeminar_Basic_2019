1bc6172b11339d265d3ad7421d258b2f426232aa
jjuncoder <jjuncoder@gmail.com>
2019-05-07 23:23:14 +0900

    4주차 세미나 전 프로젝트 업로드 및 정리

---
+++ 4주차/FlappyYonsei/Assets/Script/CameraScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime;
+    private Vector3 velocity;
+    private float yMax = 1.0f;
+    private float yMin = -1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = target.position;
+
+        // Define a target position above and behind the target transform
+        //Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+        Vector3 targetPos = new Vector3(target.position.x, Mathf.Clamp(target.position.y, yMin, yMax), -10);
+
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
---
+++ 4주차/FlappyYonsei/Assets/Script/PlayerScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace complete
+{
+    public class PlayerScript : MonoBehaviour
+    {
+        public GameObject gameOverImage;
+
+        public float kickForce_X = 100.0f;
+        public float kickForce_Y = 100.0f;
+        public float imageTime = 1.0f;
+
+        Rigidbody2D rb2d;
+
+        Animator anim;
+
+        CapsuleCollider2D bx2d;
+
+        Transform playerDieAnimObject;
+        AudioSource[] sounds;
+        AudioSource dieAudio;
+        AudioSource wingAudio;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+            anim = GetComponent<Animator>();
+            playerDieAnimObject = transform.GetChild(0);
+            playerDieAnimObject.gameObject.SetActive(false);
+            sounds = GetComponents<AudioSource>();
+            dieAudio = sounds[0];
+            wingAudio = sounds[1];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb2d.AddForce(new Vector2(1 * kickForce_X , 1 * kickForce_Y));
+                anim.Play("PlayerFlap2");
+                wingAudio.Play();
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            rb2d.freezeRotation = true;
+            playerDieAnimObject.gameObject.SetActive(true);
+            GetComponent<Collider2D>().enabled = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+            dieAudio.Play();
+            anim.SetTrigger("PlayerDie");
+            gameOverImage.SetActive(true);
+        }
+    }
+}
